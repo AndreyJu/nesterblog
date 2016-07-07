0e30ebf7f38c48f19d8a93e1abbc990716419bce
AndrewZhu <AndrewZhulanov89@gmail.com>
2016-07-07 03:02:28 +0300

    Add Model and Controller for Comments

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160706003115) do
+ActiveRecord::Schema.define(version: 20160706233153) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "parent_id"
+    t.boolean  "parent_status", default: false
+    t.text     "body",          default: ""
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
